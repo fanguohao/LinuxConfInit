f5f7bb1f53775fbb69005063c4881075a63fc0a3
finer <you@example.com>
2022-09-28 21:42:14 +0800

    first commit

---
+++ systemIni.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# add heart beat to ssh connection
+echo "start add heart beat to ssh connection"
+echo ClientAliveInterval 30 >>/etc/ssh/sshd_config 2>&1 > /dev/null
+echo ClientAliveCountMax 6 >> /etc/ssh/sshd_config 2>&1 > /dev/null
+
+# change default ssh port
+read -p "Input new ssh port:" sshPort
+echo "start change default ssh port"
+sed -i  "s/#Port 22/Port ${sshPort}/" /etc/ssh/sshd_config
+sed -i  "s/Port \([0-9]\{2,\}\)/Port ${sshPort}/" /etc/ssh/sshd_config
