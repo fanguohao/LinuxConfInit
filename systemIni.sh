92f65aa13feba6b348ebaf33f4e528969da16342
finer <you@example.com>
2022-09-29 19:58:18 +0800

    change vim space setting

--- systemIni.sh
+++ systemIni.sh
@@ -10,3 +10,9 @@ read -p "Input new ssh port:" sshPort
 echo "start change default ssh port"
 sed -i  "s/#Port 22/Port ${sshPort}/" /etc/ssh/sshd_config
 sed -i  "s/Port \([0-9]\{2,\}\)/Port ${sshPort}/" /etc/ssh/sshd_config
+
+# change vim setting
+echo "set ts=2 sw=2"> /etc/vim/vimrc 1> /dev/null
+
+
+echo"setting finished"
